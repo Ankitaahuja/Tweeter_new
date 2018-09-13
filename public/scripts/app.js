4fe397f641a069b0ea73c3733dbcd01d010fde47
ankitaahuja111 <ankitaahuja111@gmail.com>
2018-09-13 15:03:23 +0000

    minor format changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,163 +1,159 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-function convertTimeToString(time){
+function convertTimeToString(time) {
   var cd = 24 * 60 * 60 * 1000,
-      ch = 60 * 60 * 1000,
-      d = Math.floor(time / cd),
-      h = Math.floor( (time - d * cd) / ch),
-      m = Math.round( (time - d * cd - h * ch) / 60000),
-      pad = function(n){ return n < 10 ? '0' + n : n; };
-if( m === 60 ){
-  h++;
-  m = 0;
-} 
-if( h === 24 ){
-  d++;
-  h = 0;
-}
-return d +" days ago";
+    ch = 60 * 60 * 1000,
+    d = Math.floor(time / cd),
+    h = Math.floor((time - d * cd) / ch),
+    m = Math.round((time - d * cd - h * ch) / 60000),
+    pad = function (n) {
+      return n < 10 ? '0' + n : n;
+    };
+  if (m === 60) {
+    h++;
+    m = 0;
+  }
+  if (h === 24) {
+    d++;
+    h = 0;
+  }
+  return d + " days ago";
 };
 
+const data = [{
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
-    const data = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": {
-              "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-              "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-              "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": {
-              "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-              "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-              "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        },
-        {
-          "user": {
-            "name": "Johann von Goethe",
-            "avatars": {
-              "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-              "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-              "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-            },
-            "handle": "@johann49"
-          },
-          "content": {
-            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-          },
-          "created_at": 1461113796368
-        }
-      ];
-      
- function renderTweets(tweets) {
+function renderTweets(tweets) {
   tweets.sort((a, b) => b.created_at - a.created_at);
-        for (let tweet of tweets) {
-        var currentTime = Date.now();
-        var tweetTime = tweet.created_at;
-        var displayTime = currentTime - tweetTime;
-      
-          let $eachTweet = createTweetElement(tweet, convertTimeToString(displayTime));
-            $('#tweets-container').append($eachTweet);
-            
-        // loops through tweets, calls createTweetElement for each tweet
-          // takes return value and appends it to the tweets container
-        }
+  for (let tweet of tweets) {
+    var currentTime = Date.now();
+    var tweetTime = tweet.created_at;
+    var displayTime = currentTime - tweetTime;
+
+    let $eachTweet = createTweetElement(tweet, convertTimeToString(displayTime));
+    $('#tweets-container').append($eachTweet);
+
+    // loops through tweets, calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  }
 }
 
 
-  function createTweetElement(tweet, displayTime){ //create each tweet and its structure(HTML)
-    let username = tweet.user.name;
-    let handle = tweet.user.handle;
-    let profileImg = tweet.user.avatars.small;
-    let tweetContent = tweet.content.text;
-    // let timeStamp = tweet.created_at;
+function createTweetElement(tweet, displayTime) { //create each tweet and its structure(HTML)
+  let username = tweet.user.name;
+  let handle = tweet.user.handle;
+  let profileImg = tweet.user.avatars.small;
+  let tweetContent = tweet.content.text;
+  // let timeStamp = tweet.created_at;
 
-    
 
-    var $tweet = $("<article>").addClass("tweet")
+
+  var $tweet = $("<article>").addClass("tweet")
     .append($("<header>").addClass("tweet-header")
       .append($("<img>").addClass("profile-img").attr("src", profileImg))
       .append($("<h2>").addClass("user-name").text(username))
       .append($("<h4>").text(handle))
     )
     .append($("<div>").addClass("tweet-text")
-      .append($("<p>").text(tweetContent)) 
-    )// .text also helps to escapes unsafe characters by literally converting it into string, so it is safe to use with untrusted text. 
+      .append($("<p>").text(tweetContent))
+    ) // .text also helps to escapes unsafe characters by literally converting it into string, so it is safe to use with untrusted text. 
     .append($("<div>").addClass("icons")
       .append($('<i class="fas fa-flag">'))
       .append($('<i class="fas fa-retweet">'))
-        .append($('<i class="fas fa-heart">'))
+      .append($('<i class="fas fa-heart">'))
     )
     .append($("<footer>").addClass("tweet-footer").text(displayTime))
-    return $tweet;
-  }
+  return $tweet;
+}
 
-  // renderTweets(data);
-  $(document).ready(function() {
+// renderTweets(data);
+$(document).ready(function () {
 
-  $(".new-tweet form").on("submit", function(ev) {
+  $(".new-tweet form").on("submit", function (ev) {
     ev.preventDefault();
-    if($(".tweet-content").val().length > 140){
+    if ($(".tweet-content").val().length > 140) {
       $(".error-message").text("Exceed the maximum word limit!").slideDown();
-    }else if($(".tweet-content").val().length == 0){
+    } else if ($(".tweet-content").val().length == 0) {
       $(".error-message").text("Tweet cannot be empty!").slideDown();
-    }else{
+    } else {
       $.ajax({
-        url: "/tweets",  //post all the new tweets in the path
+        url: "/tweets", //post all the new tweets in the path
         method: "POST",
         data: $(this).serialize()
-      }).then(function() {
+      }).then(function () {
         loadTweets()
         $(".tweet-content").val("");
       })
-    // console.log("new-tweet", $(this).serialize());
-     }   
+      // console.log("new-tweet", $(this).serialize());
+    }
   })
 
 
   loadTweets();
 
-  function loadTweets(){
+  function loadTweets() {
     $('.tweet').remove(); //remove the old tweets from the data to avoid the repetition. 
     $.ajax({
-      url: "/tweets",   //will show all the tweets to this path
+      url: "/tweets", //will show all the tweets to this path
       method: "GET"
-    }).then(function(tweets) { //save all the tweets and send this tweets when the renderTweets is called
+    }).then(function (tweets) { //save all the tweets and send this tweets when the renderTweets is called
       renderTweets(tweets)
     })
   }
 
 
 
-  $( ".compose-button" ).click(function() {
-    $( ".new-tweet" ).slideToggle("slow", function() {
-      $( ".new-tweet textarea" ).focus();
+  $(".compose-button").click(function () {
+    $(".new-tweet").slideToggle("slow", function () {
+      $(".new-tweet textarea").focus();
     });
   });
 
-  $( ".tweet-button" ).click(function() {
-    $( ".error-message" ).hide();
-    });
+  $(".tweet-button").click(function () {
+    $(".error-message").hide();
+  });
 
 });
 
