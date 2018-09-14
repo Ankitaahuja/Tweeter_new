a1760aab54f48b4cd512e53fbfbda00daa55ad95
ankitaahuja111 <ankitaahuja111@gmail.com>
2018-09-14 15:01:06 +0000

    made changes in background color of new-tweet once it crosses the  limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,25 +4,24 @@ var pendingCharCount = 0;
 
 
 
-$(document).ready(function() {
-    console.log("Hi, I am working Now!");
+$(document).ready(function () {
+  console.log("Hi, I am working Now!");
 
-      $('.tweet-content').on('input', function() {
-        presentCharValue = this.value.length; //The this keyword is a reference to the button
-       pendingCharCount = totalAllowedChar - presentCharValue
-      console.log(pendingCharCount);
-       
-        $(".counter").text(pendingCharCount);
-       
-        if (pendingCharCount < 0 ) {
-         $(".counter").addClass("redColor");
-        }
-        else {
-         $(".counter").removeClass("redColor");
-        }
+  $('.tweet-content').on('input', function () {
+    presentCharValue = this.value.length; //The this keyword is a reference to the button
+    pendingCharCount = totalAllowedChar - presentCharValue
+    console.log(pendingCharCount);
 
-    });
-      
+    $(".counter").text(pendingCharCount);
+
+    if (pendingCharCount < 0) {
+      $(".counter").addClass("redColor");
+      $(".new-tweet").addClass("pinkBackgroundColor");
+    } else {
+      $(".counter").removeClass("redColor");
+      $(".new-tweet").removeClass("pinkBackgroundColor");
+    }
+
   });
 
- 
+});
